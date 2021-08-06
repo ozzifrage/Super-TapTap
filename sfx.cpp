88795439772409e199c48e30290491852f5924b0
Ryan Williams <40898444+watermelonslice@users.noreply.github.com>
2021-08-05 21:36:51 -0400

    SFX
    added new area for sounds in code

---
+++ sfx.cpp
@@ -0,0 +1,26 @@
+#include "sfx.h"
+
+//Put this in your .cpp file :
+const Gamebuino_Meta::Sound_FX sfx_0[] = {
+	{Gamebuino_Meta::Sound_FX_Wave::SQUARE,0,107,0,-56,56,8},
+};
+
+//Put this in your .cpp file :
+const Gamebuino_Meta::Sound_FX sfx_1[] = {
+	{Gamebuino_Meta::Sound_FX_Wave::SQUARE,0,255,-12,-104,100,10},
+};
+
+//Put this in your .cpp file :
+const Gamebuino_Meta::Sound_FX sfx_2[] = {
+	{Gamebuino_Meta::Sound_FX_Wave::NOISE,0,121,-4,-6,96,10},
+};
+
+//Put this in your .cpp file :
+const Gamebuino_Meta::Sound_FX sfx_3[] = {
+	{Gamebuino_Meta::Sound_FX_Wave::NOISE,0,108,0,14,96,66},
+};
+
+//Put this in your .cpp file :
+const Gamebuino_Meta::Sound_FX sfx_5[] = {
+	{Gamebuino_Meta::Sound_FX_Wave::NOISE,0,108,-3,0,144,9},
+};
---
+++ sfx.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Gamebuino-Meta.h>
+
+//Put this in your .h file :
+extern const Gamebuino_Meta::Sound_FX sfx_0[];
+
+//Put this in your .h file :
+extern const Gamebuino_Meta::Sound_FX sfx_1[];
+
+//Put this in your .h file :
+extern const Gamebuino_Meta::Sound_FX sfx_2[];
+
+//Put this in your .h file :
+extern const Gamebuino_Meta::Sound_FX sfx_3[];
+
+//Put this in your .h file :
+extern const Gamebuino_Meta::Sound_FX sfx_5[];
